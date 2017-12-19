b5529a0ff2d5c5501a3cc8d4a91aa245d25562af
Brizeno <raissa.costa.brizeno@accenture.com>
2017-12-18 23:34:03 -0300

    first commit

---
+++ calculator.rb
@@ -0,0 +1,47 @@
+def sum(x, y)
+  resultado = x+y
+  return resultado
+end
+def subtraction(x, y)
+  resultado = x-y
+  return resultado
+end
+def division(x, y)
+  resultado = x/y
+  return resultado
+end
+def multiplication(x, y)
+  resultado = x*y
+  return resultado
+end
+
+def test
+  begin
+    puts sum(1, 1) 
+    puts sum(2,"a")
+  rescue Exception => e
+    print "error in sum function, " + e.to_s
+  end
+  
+  begin
+    puts subtraction(2, 1) 
+  rescue Exception => e
+    print "error in subtraction function, " + e.to_s
+  end
+  
+  begin
+    puts division(10, 2) 
+    puts division(10,0)
+  rescue Exception => e
+    print "error in division function, " + e.to_s
+  end
+  
+  begin
+    puts multiplication(5, 2)
+  rescue Exception => e
+    print "error in multiplication function, " + e.to_s
+  end
+
+end
+
+test
